e3394ae5b6b8ca09ab173127cd5462139e0e9494
Gavin <86824319+GavinITP@users.noreply.github.com>
2023-04-10 21:25:36 +0700

    Fetch data from API

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,34 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const Card = () => {
+  const [id, setId] = useState("");
+  const [advice, setAdvice] = useState("");
+
+  const fetchAdvice = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((res) => {
+        setId(res.data.slip.id);
+        setAdvice(res.data.slip.advice);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
   return (
     <div className="w-full bg-slate-700 text-slate-200 rounded-xl relative top-32 text-center px-4 hover:scale-[102%] ease-out duration-300 ">
       <h1 className="text-xs text-emerald-400 uppercase font-bold py-8">
-        Advice #117
+        Advice #{id}
       </h1>
-      <p className="font-bold text-2xl">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, nobis.
-      </p>
-      <button className="rounded-[50%] w-16 h-16 bg-emerald-400 hover:bg-emerald-500">
+      <p className="font-bold text-2xl">{advice}</p>
+      <button
+        onClick={fetchAdvice}
+        className="rounded-[50%] w-16 h-16 bg-emerald-400 hover:bg-emerald-500"
+      >
         Random
       </button>
     </div>
