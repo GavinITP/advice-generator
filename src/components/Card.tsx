c20a8306ddecd112a4bb89342187979f18df8880
Gavin <86824319+GavinITP@users.noreply.github.com>
2023-04-10 21:30:41 +0700

    Implement loading state

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,20 +4,31 @@ import { useEffect, useState } from "react";
 const Card = () => {
   const [id, setId] = useState("");
   const [advice, setAdvice] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchAdvice = () => {
+    setIsLoading(true);
     axios
       .get("https://api.adviceslip.com/advice")
       .then((res) => {
         setId(res.data.slip.id);
         setAdvice(res.data.slip.advice);
+        setIsLoading(false);
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        console.log(err);
+        setIsLoading(false);
+      });
   };
 
   useEffect(() => {
     fetchAdvice();
   }, []);
+
+  if (isLoading)
+    return (
+      <p className="text-center text-slate-100 relative top-40">Loading...</p>
+    );
 
   return (
     <div className="w-full bg-slate-700 text-slate-200 rounded-xl relative top-32 text-center px-4 hover:scale-[102%] ease-out duration-300 ">
